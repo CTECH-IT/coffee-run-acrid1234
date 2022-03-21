44e53756f24c52274b458b97dcade773425ea5ae
= <=>
2022-03-21 09:20:35 -0500

    added checklist and implemented

---
+++ scripts/checklist.js
@@ -0,0 +1,63 @@
+(function (window) {
+    'use strict';
+    let App = window.App || {};
+    let $ = window.jQuery;
+    function Checklist(selector) {
+        if (!selector) {
+            throw new Error('No selector provided');
+        }
+        this.$element = $(selector);
+        if (this.$element.length ===0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+    CheckList.prototype.addRow = function (coffeeOrder) {
+        this.removeRow(coffeeOrder.emailAddress);
+        var rowElement = new Row(coffeeOrder);
+        this.$element.append(rowElement.$element);
+    }
+
+    CheckList.prototype.removeRow = function (email) {
+        this.$element
+        .find('[value="' + email + '"]')
+        .closest('[data-coffee-order="checkbox"]')
+        .remove();
+    };
+
+    CheckList.prototype.addClickHandler = function (func) {
+        this.$element.on('click', 'input', function (event) {
+            var email = event.target.value;
+            this.removeRow(email);
+            func(email);
+        }.bind(this));
+    };
+
+    function Row(coffeeOrder) {
+        let $div = $('<div></div>', {
+            'data-coffee-order': 'checkbox', 'class': 'checkbox'
+        });
+        let $label = $('<label></label>');
+
+        let $checkbox = $('<input></input>', {
+            type: 'checkbox',
+            value: coffeeOrder.emailAddress
+        });
+
+        let description = coffeeOrder.coffee + ', ';
+        if (coffeeOrder.flavor) {
+            description += coffeeOrder.flavor + ' ';
+        }
+        description += coffeeOrder.coffee + ', ';
+        description += ' (' + coffeeOrder.emailAddress + ')';
+        description += ' [' + coffeeOrder.strength + 'x]';
+
+        $label.append($checkbox);
+        $label.append(description);
+        $div.append($label);
+
+        this.$element = $div;
+    }
+    App.CheckList = CheckList;
+    window.App = App;
+})(window);
--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,26 @@
     'use strict';
 
     const FORM_SELECTOR = '[data-coffee-order="form"]';
+    const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
     let FormHandler = App.Formhandler;
+    let CheckList = App.CheckList;
 
     let myTruck = new Truck('12345', new DataStore());
-
     window.myTruck = myTruck;
 
+    let checkList = new CheckList(CHECKLIST_SELECTOR);
+
     let formHandler = new FormHandler(FORM_SELECTOR);
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-    console.log(formHandler);
+
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.createOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    })
+
+    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
 
 })(window);
