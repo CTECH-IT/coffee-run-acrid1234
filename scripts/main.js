1eb5960e323c621594e6e870607c812fd3662e97
= <=>
2022-04-04 09:21:34 -0500

    finished RemoteDataStore and everything

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,19 @@
 
     const FORM_SELECTOR = '[data-coffee-order="form"]';
     const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+    const SERVER_URL = 'https://saturn.rochesterschools.org:8080/json';
 
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
+    let RemoteDataStore = App.RemoteDataStore;
     let FormHandler = App.Formhandler;
     let CheckList = App.CheckList;
     let Validation = App.Validation;
 
-    let myTruck = new Truck('12345', new DataStore());
+    let remoteDS = new RemoteDataStore(SERVER_URL);
+
+    let myTruck = new Truck('12345', remoteDS);
     window.myTruck = myTruck;
 
     let checkList = new CheckList(CHECKLIST_SELECTOR);
